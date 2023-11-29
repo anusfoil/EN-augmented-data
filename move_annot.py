cfdb626e2b3ad87bf012c59a0d432cc3213d3723
Huan Zhang <31784445+Kristin33@users.noreply.github.com>
2023-11-29 23:10:21 +0000

    half manually checked

---
+++ move_annot.py
@@ -0,0 +1,14 @@
+# move the annotation into note 0 of the other track
+import glob
+from mido import MidiFile
+
+
+mid_annotations = glob.glob("**/*-annot.mid", recursive=True)
+
+for ma_path in mid_annotations:
+	mid = MidiFile(ma_path)
+	for message in mid.tracks[2]:
+		if message.type in ['note_on', 'note_off']:
+			message.note = 0
+
+	mid.save(ma_path)
